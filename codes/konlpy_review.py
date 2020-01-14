b06b2c6a8dc923a125d8cb3f8705f113076dc745
parkjunho12 <ghdlwnsgh12@naver.com>
2020-01-14 14:48:03 +0900

    1-2. MoviewReview konlpy_review.py 다른 분이 쓴 코드 도입 1. save 시켜보고 로드 해봐야겠다.
    문제점
    
    해결방범

---
+++ codes/konlpy_review.py
@@ -0,0 +1,101 @@
+def read_data(filename):
+    with open(filename, 'r', encoding="utf-8") as f:
+        data = [line.split('\t') for line in f.read().splitlines()]
+        # txt 파일의 헤더(id document label)는 제외하기
+        data = data[1:]
+    return data
+
+train_data = read_data('ratings_train.txt')
+test_data = read_data('ratings_test.txt')
+
+from konlpy.tag import Okt
+
+okt = Okt()
+
+
+import json
+import os
+from pprint import pprint
+
+def tokenize(doc):
+    # norm은 정규화, stem은 근어로 표시하기를 나타냄
+    return ['/'.join(t) for t in okt.pos(doc, norm=True, stem=True)]
+
+if os.path.isfile('train_docs.json'):
+    with open('train_docs.json') as f:
+        train_docs = json.load(f)
+    with open('test_docs.json') as f:
+        test_docs = json.load(f)
+else:
+    train_docs = [(tokenize(row[1]), row[2]) for row in train_data]
+    test_docs = [(tokenize(row[1]), row[2]) for row in test_data]
+    # JSON 파일로 저장
+    with open('train_docs.json', 'w', encoding="utf-8") as make_file:
+        json.dump(train_docs, make_file, ensure_ascii=False, indent="\t")
+    with open('test_docs.json', 'w', encoding="utf-8") as make_file:
+        json.dump(test_docs, make_file, ensure_ascii=False, indent="\t")
+
+
+tokens = [t for d in train_docs for t in d[0]]
+
+
+import nltk
+text = nltk.Text(tokens, name='NMSC')
+
+
+selected_words = [f[0] for f in text.vocab().most_common(10000)]
+
+def term_frequency(doc):
+    return [doc.count(word) for word in selected_words]
+
+train_x = [term_frequency(d) for d, _ in train_docs]
+test_x = [term_frequency(d) for d, _ in test_docs]
+train_y = [c for _, c in train_docs]
+test_y = [c for _, c in test_docs]
+
+
+import numpy as np
+
+x_train = np.asarray(train_x).astype('float32')
+x_test = np.asarray(test_x).astype('float32')
+
+y_train = np.asarray(train_y).astype('float32')
+y_test = np.asarray(test_y).astype('float32')
+
+
+from tensorflow.keras import models
+from tensorflow.keras import layers
+from tensorflow.keras import optimizers
+from tensorflow.keras import losses
+from tensorflow.keras import metrics
+
+model = models.Sequential()
+model.add(layers.Dense(64, activation='relu', input_shape=(10000,)))
+model.add(layers.Dense(64, activation='relu'))
+model.add(layers.Dense(1, activation='sigmoid'))
+
+model.compile(optimizer=optimizers.RMSprop(lr=0.001),
+             loss=losses.binary_crossentropy,
+             metrics=[metrics.binary_accuracy])
+
+model.fit(x_train, y_train, epochs=10, batch_size=512)
+results = model.evaluate(x_test, y_test)
+model.save('../first_model.h5')
+
+
+def predict_pos_neg(review):
+    token = tokenize(review)
+    tf = term_frequency(token)
+    data = np.expand_dims(np.asarray(tf).astype('float32'), axis=0)
+    score = float(model.predict(data))
+    if(score > 0.5):
+        print("[{}]는 {:.2f}% 확률로 긍정 리뷰이지 않을까 추측해봅니다.^^\n".format(review, score * 100))
+    else:
+        print("[{}]는 {:.2f}% 확률로 부정 리뷰이지 않을까 추측해봅니다.^^;\n".format(review, (1 - score) * 100))
+
+
+predict_pos_neg("올해 최고의 영화! 세 번 넘게 봐도 질리지가 않네요.")
+predict_pos_neg("배경 음악이 영화의 분위기랑 너무 안 맞았습니다. 몰입에 방해가 됩니다.")
+predict_pos_neg("주연 배우가 신인인데 연기를 진짜 잘 하네요. 몰입감 ㅎㄷㄷ")
+predict_pos_neg("믿고 보는 감독이지만 이번에는 아니네요")
+predict_pos_neg("주연배우 때문에 봤어요")
